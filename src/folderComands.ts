6ba4465ae787b349769ee363cf65570796c62030
Armando <armando.fernandes.rodrigues@gmail.com>
2024-09-11 22:55:05 +0100

    defined arquive file

--- src/folderComands.ts
+++ src/folderComands.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as AdmZip from 'adm-zip';
 // import * as moment from 'moment';
 // const dateFormat = require('dateformat');
 // import * as admzip from 'adm-zip';
@@ -16,15 +17,25 @@ export function arquiveFolder(context: vscode.ExtensionContext, uri: vscode.Uri,
     const rootFile = path.basename(rootPath);
     const arqPath = path.join(path.dirname(rootPath), 'Arquive');
     const curTime = new Date();
-    // const curTime = new dayjs();
-    // const txtTime = curTime.format();
-    // const arqFile = `${rootFile}.old.${moment}`;
-    // outputChannel.show();
+    const txtTime:string = `${curTime.getFullYear().toString()}${curTime.getMonth().toString().padStart(2, '0')}${curTime.getDate().toString().padStart(2, '0')}_${curTime.getHours().toString().padStart(2, '0')}${curTime.getMinutes().toString().padStart(2, '0')}${curTime.getSeconds().toString().padStart(2, '0')}`;
+    // Assure existence of Arquive Folder
+    if (!fs.existsSync(arqPath)) {
+        fs.mkdirSync(arqPath, { recursive: true });
+        outputChannel.appendLine(`Created folder: ${arqPath}`);
+    } else {outputChannel.appendLine(`Already existed folder: ${arqPath}`);}
+    // Get next version number
+    let lastVersion = 0;
+    fs.readdir(arqPath, (err, files) => {
+        if (err) {
+            return;
+        }
+        lastVersion = files.filter(file => file.startsWith(rootFile)).length;
+    });
+    const nextVersion:string = `${(lastVersion + 1)}`;
+    const arqFile = `${rootFile}.old.${txtTime}[V ${nextVersion.padStart(3, '0')}].zip`;
     outputChannel.appendLine(`Folder to Arquive: ${rootPath}`);
     outputChannel.appendLine(`File to Arquive: ${rootFile}`);
     outputChannel.appendLine(`Arquive folder: ${arqPath}`);
-    outputChannel.appendLine(`Uma Data: ${Date.now}`);
-    outputChannel.appendLine(`Uma Data Formatada: ${Date.now.toString()}`);
-    outputChannel.appendLine(`Agora: ${curTime.toString()}`);
-    // outputChannel.appendLine(`Agora: ${curTime.form()}`);
+    outputChannel.appendLine(`Agora Formatado: ${txtTime}`);
+    outputChannel.appendLine(`Arquive: ${arqFile}`);
 }
