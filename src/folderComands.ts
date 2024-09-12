a8fd21a23102935a689b5e8179cdcc518293801f
Armando <armando.fernandes.rodrigues@gmail.com>
2024-09-12 01:09:18 +0100

    implemented arquive

--- src/folderComands.ts
+++ src/folderComands.ts
@@ -2,10 +2,6 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as AdmZip from 'adm-zip';
-// import * as moment from 'moment';
-// const dateFormat = require('dateformat');
-// import * as admzip from 'adm-zip';
-// import * as df from 'dateformat';
 
 export function arquiveFolder(context: vscode.ExtensionContext, uri: vscode.Uri, outputChannel: vscode.OutputChannel) {
     // Check if a folder was selected
@@ -24,18 +20,54 @@ export function arquiveFolder(context: vscode.ExtensionContext, uri: vscode.Uri,
         outputChannel.appendLine(`Created folder: ${arqPath}`);
     } else {outputChannel.appendLine(`Already existed folder: ${arqPath}`);}
     // Get next version number
-    let lastVersion = 0;
-    fs.readdir(arqPath, (err, files) => {
-        if (err) {
-            return;
+    const files = fs.readdirSync(arqPath);
+    const lastVersion = files.filter(file => file.startsWith(rootFile)).length;
+    const nextVersion = (lastVersion+1);
+    const arqFile = `${rootFile}.old.${txtTime}[V${nextVersion.toString().padStart(3, '0')}].zip`;
+    const arqFilePath = path.join(arqPath, arqFile);
+
+    // Function to create ZIP arquive from source folder
+    function zipArquiveFolder(sourceFolder:string, outputZip:string) {
+        const zipObj = new AdmZip.default();
+        // Add the main folder
+        const sourceMain:string = path.basename(sourceFolder);
+        zipObj.addLocalFolder(sourceFolder, sourceMain);
+        // Recursive AddFolder/File to Arquive
+        function addFolderToZip(folderPath:string, zipPath:string) {
+            fs.readdirSync(folderPath).forEach(file => {
+                const filePath = path.join(folderPath, file);
+                const stat = fs.statSync(filePath);
+                if (stat.isDirectory()) {
+                    // If it's a directory, then add recursively its content
+                    outputChannel.appendLine(`Add Folder ${filePath}`);
+                    zipObj.addLocalFolder(filePath, zipPath);
+                    addFolderToZip(filePath, path.join(zipPath, file));
+                } else {
+                    // If it's a file, then add it to the zip
+                    outputChannel.appendLine(`Add File ${filePath}`);
+                    zipObj.addLocalFile(filePath, zipPath);
+                }
+            });
         }
-        lastVersion = files.filter(file => file.startsWith(rootFile)).length;
-    });
-    const nextVersion:string = `${(lastVersion + 1)}`;
-    const arqFile = `${rootFile}.old.${txtTime}[V ${nextVersion.padStart(3, '0')}].zip`;
-    outputChannel.appendLine(`Folder to Arquive: ${rootPath}`);
-    outputChannel.appendLine(`File to Arquive: ${rootFile}`);
-    outputChannel.appendLine(`Arquive folder: ${arqPath}`);
-    outputChannel.appendLine(`Agora Formatado: ${txtTime}`);
-    outputChannel.appendLine(`Arquive: ${arqFile}`);
+        outputChannel.appendLine(`Recursive folder ${sourceMain}...`);
+        addFolderToZip(sourceFolder, sourceMain);
+        zipObj.writeZip(outputZip);
+    }
+    // Function to create ZIP arquive from source file
+    function zipArquiveFile(sourceFilePath:string, outputZip:string) {
+        outputChannel.appendLine(`Recursive File`);
+        const zipObj = new AdmZip.default();
+        zipObj.addLocalFile(sourceFilePath);
+        zipObj.writeZip(outputZip);
+    }
+    // Evaluate if source is file or folder
+    const statSrc = fs.statSync(rootPath);
+    if (statSrc.isDirectory()) {
+        outputChannel.appendLine(`${rootPath} is a Directory`);
+        zipArquiveFolder(rootPath, arqFilePath);
+    } else {
+        outputChannel.appendLine(`${rootPath} is a File`);
+        zipArquiveFile(rootPath, arqFilePath);
+    }
+    outputChannel.appendLine(`File/Folder "${rootPath}" arquived to ${arqFilePath}`);
 }
